23680a6e0d6648883ec650439e90e4d3f33f9ab7
tengrommel <wwwsousoucom@126.com>
2017-12-14 22:37:24 +0800

    http

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
@@ -18,9 +19,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request)  {
 	}
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params)  {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main()  {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handlerFunc)
+	router := httprouter.New()
+	router.GET("/hello/:name/spanish", Hello)
+
 	//http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", mux)
+	http.ListenAndServe(":3000", router)
 }
